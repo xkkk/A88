efb41863de5a1f6036b06751d2fa37c1e471a7d9
徐坤 <xuk@zzkqdc.com>
2022-09-05 23:54:51 +0800

    a88 混动版 语音

--- app/src/main/java/com/baorun/handbook/a6v/feature/warn/SchemeWarnActivity.kt
+++ app/src/main/java/com/baorun/handbook/a6v/feature/warn/SchemeWarnActivity.kt
@@ -3,55 +3,42 @@ package com.baorun.handbook.a6v.feature.warn
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.baorun.handbook.a6v.MainActivity
-import com.baorun.handbook.a6v.R
 import com.baorun.handbook.a6v.databinding.ActivitySchemeWarnBinding
-import com.baorun.handbook.a6v.utils.goActivity
 import com.baorun.handbook.a6v.utils.toSupportJavaScript
 
 
 /**
  * 通过scheme 打开app 跳转的页面
  */
-class SchemeWarnActivity:AppCompatActivity() {
+class SchemeWarnActivity: AppCompatActivity() {
 
     private lateinit var viewBinding: ActivitySchemeWarnBinding
 
-    private val viewModel: WarnViewModel by viewModels()
+    private val viewModel:WarnViewModel by viewModels()
 
+    private var type = 1 // 1-告警 2-语音
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivitySchemeWarnBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         viewBinding.webView.toSupportJavaScript()
-        viewBinding.backTv.apply {
-            text = "回到首页"
-            val drawable = ContextCompat.getDrawable(this@SchemeWarnActivity, R.drawable.ic_back)!!
-            drawable.setBounds(0,0,30,30)
-            compoundDrawablePadding = 10
-            setCompoundDrawables(drawable,null,null,null)
-            setOnClickListener {
-                goActivity(MainActivity::class.java,true)
-            }
-        }
-
 
         parserData(intent)
 
         viewModel.warnData.observe(this){
             if(it!=null){
                 showEmpty(false)
-                viewBinding.webView.loadUrl("file:///android_asset/warn${it.htmlUrl}")
+                if(type == 2) {
+                    viewBinding.webView.loadUrl("file:///android_asset/voice${it.htmlUrl}")
+                }else{
+                    viewBinding.webView.loadUrl("file:///android_asset/warn${it.htmlUrl}")
+                }
             }else{
                 showEmpty()
             }
         }
-
-
     }
 
 
@@ -70,7 +57,13 @@ class SchemeWarnActivity:AppCompatActivity() {
                 val split = path.split("/")
                 if(split.isNotEmpty()){
                     val id = split.last().trim()
-                    viewModel.getWarnById(id)
+                    if(split.contains("voice")) {
+                        type=2
+//                        viewModel.getVoiceById("yy_$id")
+                    }else{
+                        type=1
+                        viewModel.getWarnById("gj_$id")
+                    }
                 }else{
                     showEmpty()
                 }
@@ -84,6 +77,11 @@ class SchemeWarnActivity:AppCompatActivity() {
 
     }
 
+    //    override fun onBackPressed() {
+//        super.onBackPressed()
+//        goActivity(MainActivity::class.java,true)
+//        finish()
+//    }
     private fun showEmpty(visible:Boolean = true){
         if(visible) {
             viewBinding.webView.visibility = View.GONE
@@ -92,12 +90,5 @@ class SchemeWarnActivity:AppCompatActivity() {
             viewBinding.webView.visibility = View.VISIBLE
             viewBinding.empty.root.visibility = View.GONE
         }
-    }
-
-    override fun onDestroy() {
-        viewBinding.webView.removeAllViews()
-        viewBinding.webView.destroy()
-        ((viewBinding.webView.parent) as ViewGroup).removeAllViews()
-        super.onDestroy()
     }
 }
